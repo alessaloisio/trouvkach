0a9910c6ceccab18dd087dd673d227b11194cf74
Alessandro Aloisio <alessaloisio@gmail.com>
2019-12-02 14:15:01 +0100

    add project init

---
+++ src/client/components/hello.js
@@ -0,0 +1,20 @@
+/* becodeorg/trouvkach
+ *
+ * /src/client/components/hello.js - Hello Component
+ *
+ * coded by leny@BeCode
+ * started at 06/09/2019
+ */
+
+import * as React from "react";
+
+const HelloWorld = () => (
+    <div>
+        <h1>{"Hello, world!"}</h1>
+        <hr />
+        <h2> {"salut c'est cool"}</h2>
+        <small>{"becode/trouvkach"}</small>
+    </div>
+);
+
+export default HelloWorld;
