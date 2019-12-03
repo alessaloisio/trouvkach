e1482126666eb19d3e23c0baff4525061758bb73
Alessandro Aloisio <alessaloisio@gmail.com>
2019-12-03 14:03:35 +0100

    init project

---
+++ src/client/app.js
@@ -0,0 +1,14 @@
+/* becodeorg/trouvkach
+ *
+ * /src/client/app.js - Client entry point
+ *
+ * coded by leny@BeCode
+ * started at 06/09/2019
+ */
+
+import * as React from "react";
+import ReactDOM from "react-dom";
+
+import HelloWorld from "./components/hello";
+
+ReactDOM.render(<HelloWorld />, document.querySelector("#app"));
