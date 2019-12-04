ad37c26ef043106bad6a267afc4560bb459b8e22
Heusic0m <Heusicom.Maxime@hotmail.com>
2019-12-04 10:17:20 +0100

    mongodb connection and get docs from collection

--- src/server/index.js
+++ src/server/index.js
@@ -13,6 +13,40 @@ const {APP_PORT} = process.env;
 
 const app = express();
 
+// MONGODB
+const mongo = require("mongodb").MongoClient;
+const assert = require("assert");
+
+const url = "mongodb://dev:dev@mongo:27017";
+const dbName = "trouvkash";
+
+mongo.connect(url, (err, client) => {
+    assert.equal(null, err);
+    console.log("Connected successfully to server");
+
+    const db = client.db(dbName);
+
+    // Banks
+    const Banks = db.collection("banks");
+    // eslint-disable-next-line no-shadow
+    Banks.find({}).toArray((err, docs) => {
+        assert.equal(null, err);
+        console.log("Found the following records");
+        console.log(docs);
+    });
+
+    // Terminals
+    const Terminals = db.collection("terminals");
+    // eslint-disable-next-line no-shadow
+    Terminals.find({}).toArray((err, docs) => {
+        assert.equal(null, err);
+        console.log("Found the following records");
+        console.log(docs);
+    });
+
+    // client.close();
+});
+
 app.use(express.static(path.resolve(__dirname, "../../bin/client")));
 
 app.get("/hello", (req, res) => {
