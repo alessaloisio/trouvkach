8cbae8064d5956933294d4860af0276b790102f1
ismail <ismail.elydi@gmail.com>
2019-12-05 09:47:21 +0100

    je sais plus

--- src/server/index.js
+++ src/server/index.js
@@ -5,9 +5,11 @@
  * coded by leny@BeCode
  * started at 06/09/2019
  */
-
+//express framework permet de créé serveur node js
 import express from "express";
 import path from "path";
+
+const mongoose = require("mongoose");
 
 const {APP_PORT} = process.env;
 
@@ -23,3 +25,10 @@ app.get("/hello", (req, res) => {
 app.listen(APP_PORT, () =>
     console.log(`🚀 Server is listening on port ${APP_PORT}.`),
 );
+
+const mongo = mongoose.connect("mongodb://dev:dev@mongo:27017/", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+});
+
+console.log(mongo);
